b69a916d2333fb3ba37f35db97e8866cf32559a0
charlottesuarez22 <charlotte.suarez@yahoo.fr>
2020-03-06 11:25:51 +0100

    ajout quiz/tips

---
+++ app/src/main/java/fr/isen/berton/projectapp/QuizActivity.kt
@@ -0,0 +1,44 @@
+package fr.isen.berton.projectapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_quiz.*
+
+class QuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+
+        radioButton1.setOnClickListener {
+            if (radioButton1.isChecked === true ) {
+                radioButton2.isChecked = false
+                radioButton3.isChecked = false
+            }
+        }
+
+        radioButton2.setOnClickListener {
+            if (radioButton2.isChecked === true ) {
+                radioButton1.isChecked = false
+                radioButton3.isChecked = false
+            }
+        }
+
+        radioButton3.setOnClickListener {
+            if (radioButton3.isChecked === true ) {
+                radioButton1.isChecked = false
+                radioButton2.isChecked = false
+            }
+        }
+
+        validateButton.setOnClickListener {
+               val intent = Intent(this, QuizActivity::class.java)
+               intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+               startActivity(intent)
+           }
+       }
+
+
+}
