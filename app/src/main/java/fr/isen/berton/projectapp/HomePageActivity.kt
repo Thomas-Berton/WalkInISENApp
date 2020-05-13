e4444df868084c5272a6b11b52a1c8be1c7633d3
charlottesuarez22 <charlotte.suarez@yahoo.fr>
2020-05-13 16:55:41 +0200

    Quiz quotidien terminé, envoi des points sur la home page + sauvegarde

--- app/src/main/java/fr/isen/berton/projectapp/HomePageActivity.kt
+++ app/src/main/java/fr/isen/berton/projectapp/HomePageActivity.kt
@@ -2,6 +2,8 @@ package fr.isen.berton.projectapp
 
 import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,15 +18,13 @@ import kotlinx.android.synthetic.main.activity_home_page.*
 class HomePageActivity : AppCompatActivity() {
     var currentProgress = 0
     var etage = 0
-    var answersPoint: Int = 0
     var thisActivity = this
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
-        //resetEtage()
-
+        //resetPoints()
         val sharedPref = thisActivity?.getPreferences(Context.MODE_PRIVATE) ?: return
         etage = sharedPref.getInt("points", 0)
         increaseProgress()
@@ -33,22 +33,20 @@ class HomePageActivity : AppCompatActivity() {
         RankPicture.setOnClickListener {}
         AwarenessPicture.setOnClickListener {
             val intent = Intent(this, QuizActivity::class.java)
-            // intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
-            //Log.d("test","Les points sont : "+etage)
-
             startActivityForResult(intent, 0)
 
 
         }
         GamePicture.setOnClickListener { }
-
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (resultCode == RESULT_OK) {
+            //RECUPERATION DES POINTS DU QUIZ avec GETINTEXTRA
             etage += data?.getIntExtra("points", 0)!!
+            //Sauvegarde dans un shared preference
             val sharedPref = thisActivity?.getPreferences(Context.MODE_PRIVATE) ?: return
             with (sharedPref.edit()) {
                 putInt("points", etage)
@@ -58,13 +56,6 @@ class HomePageActivity : AppCompatActivity() {
         }
     }
 
-    fun resetEtage(){
-        val sharedPref = thisActivity?.getPreferences(Context.MODE_PRIVATE) ?: return
-        with (sharedPref.edit()) {
-            putInt("points", 0)
-            commit()
-        }
-    }
 
     fun increaseProgress() {
         if (currentProgress < 100) {
@@ -73,9 +64,16 @@ class HomePageActivity : AppCompatActivity() {
         }
     }
 
-    fun getPoints() {
-        answersPoint += intent.getSerializableExtra("points") as Int
-        etage = etage + answersPoint
+    fun resetPoints(){
+        val sharedPref = thisActivity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putInt("points", 0)
+            commit()
+        }
     }
 
+    /*fun getPoints() {
+        answersPoint += intent.getSerializableExtra("points") as Int
+        etage = etage + answersPoint
+    }*/
 }
--- app/src/main/java/fr/isen/berton/projectapp/QuizActivity.kt
+++ app/src/main/java/fr/isen/berton/projectapp/QuizActivity.kt
@@ -13,6 +13,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_quiz_done.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.random.Random
@@ -32,190 +33,170 @@ class QuizActivity : AppCompatActivity() {
     var oldDate = 0
     var date = 0
     var currentDate = Date()
-    var newDate = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz)
-
-
-        goodAnswer.isVisible = false
-        badAnswer.isVisible = false
-        pengouinGood.isVisible = false
-
         date = displayDate()
-        Toast.makeText(this,"Nous sommes le : "+date, Toast.LENGTH_LONG).show()
+        //Toast.makeText(this,"Nous sommes le : "+date, Toast.LENGTH_LONG).show()
         val sharedPref = thisActivity?.getPreferences(Context.MODE_PRIVATE) ?: return
-        oldDate = sharedPref.getInt("date",date)
+        oldDate = sharedPref.getInt("date",0)
         Log.d("testPref","La date est : "+oldDate)
 
-        /*date = data?.getIntExtra("date", 0)!!
-        val sharedPref = thisActivity?.getPreferences(Context.MODE_PRIVATE) ?: return
-        with (sharedPref.edit()) {
-            putInt("date", date)
-            commit()
-        }*/
+        val sharedPrefIndex = thisActivity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        index = sharedPrefIndex.getInt("index",0)
 
+        if (oldDate < date){
+            setContentView(R.layout.activity_quiz)
+            goodAnswer.isVisible = false
+            badAnswer.isVisible = false
+            pengouinGood.isVisible = false
+            HomePagePicture.setOnClickListener{
+                val intent = Intent(this, HomePageActivity::class.java)
+                //ENVOI DES POINTS DU QUIZ VERS LA HOME PAGE avec PUTEXTRA
+                intent.putExtra("points", answersPoints)
+                setResult(RESULT_OK, intent)
 
+                finish()
 
-        if (oldDate < newDate)
-        {
-            Log.d("testdate","ok")
-        }
-
-        HomePagePicture.setOnClickListener{
-            val intent = Intent(this, HomePageActivity::class.java)
-            //intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
-            intent.putExtra("points", answersPoints)
-            //Log.d("test2", "Les points sont : " + answersPoints)
-            setResult(RESULT_OK, intent)
-
-            finish()
-        }
-
-
-        val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("tips")
-        val myRefQ = database.getReference("quiz/questions")
-        val myRefR1 = database.getReference("quiz/réponse1")
-        val myRefR2 = database.getReference("quiz/réponse2")
-        val myRefRV = database.getReference("quiz/réponseV")
-
-        myRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                dataSnapshot.children.forEach {
-                    val value = it.getValue(String::class.java)
-                    Log.d(TAG, "Value is: $value")
-                    tipsList.add(value ?: "")
-                }
-                displayRandomTips()
             }
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.w(TAG, "Failed to read value.", error.toException())
-            }
-        })
 
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("tips")
+            val myRefQ = database.getReference("quiz/questions")
+            val myRefR1 = database.getReference("quiz/réponse1")
+            val myRefR2 = database.getReference("quiz/réponse2")
+            val myRefRV = database.getReference("quiz/réponseV")
 
-        myRefQ.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                dataSnapshot.children.forEach {
-                    val value = it.getValue(String::class.java)
-                    questionList.add(value ?: "")
+            myRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    dataSnapshot.children.forEach {
+                        val value = it.getValue(String::class.java)
+                        Log.d(TAG, "Value is: $value")
+                        tipsList.add(value ?: "")
+                    }
+                    displayRandomTips()
                 }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.w(TAG, "Failed to read value.", error.toException())
+                }
+            })
+
+
+            myRefQ.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    dataSnapshot.children.forEach {
+                        val value = it.getValue(String::class.java)
+                        questionList.add(value ?: "")
+                    }
+                    displayRandomQuestion(index)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.w(TAG, "Failed to read value.", error.toException())
+                }
+            })
+
+            myRefR1.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    dataSnapshot.children.forEach {
+                        val value = it.getValue(String::class.java)
+                        r1List.add(value ?: "")
+                    }
+                    displayRandomAnswer1(index)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.w(TAG, "Failed to read value.", error.toException())
+                }
+            })
+
+            myRefR2.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    dataSnapshot.children.forEach {
+                        val value = it.getValue(String::class.java)
+                        r2List.add(value ?: "")
+                    }
+                    displayRandomAnswer2(index)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.w(TAG, "Failed to read value.", error.toException())
+                }
+            })
+
+            myRefRV.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    dataSnapshot.children.forEach {
+                        val value = it.getValue(String::class.java)
+                        rvList.add(value ?: "")
+                    }
+                    displayRandomAnswerV(index)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.w(TAG, "Failed to read value.", error.toException())
+                }
+            })
+
+            r1.setOnClickListener {
+                if (r1.isChecked === true) {
+                    r2.isChecked = false
+                    r3.isChecked = false
+                }
+            }
+
+            r2.setOnClickListener {
+                if (r2.isChecked === true) {
+                    r1.isChecked = false
+                    r3.isChecked = false
+                }
+            }
+
+            r3.setOnClickListener {
+                if (r3.isChecked === true) {
+                    r1.isChecked = false
+                    r2.isChecked = false
+                }
+            }
+
+            validateButton.setOnClickListener {
+                //index++ appel a la fct des jours
                 displayRandomQuestion(index)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.w(TAG, "Failed to read value.", error.toException())
-            }
-        })
-
-        myRefR1.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                dataSnapshot.children.forEach {
-                    val value = it.getValue(String::class.java)
-                    r1List.add(value ?: "")
-                }
                 displayRandomAnswer1(index)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.w(TAG, "Failed to read value.", error.toException())
-            }
-        })
-
-        myRefR2.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                dataSnapshot.children.forEach {
-                    val value = it.getValue(String::class.java)
-                    r2List.add(value ?: "")
-                }
                 displayRandomAnswer2(index)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.w(TAG, "Failed to read value.", error.toException())
-            }
-        })
-
-        myRefRV.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                dataSnapshot.children.forEach {
-                    val value = it.getValue(String::class.java)
-                    rvList.add(value ?: "")
-                }
                 displayRandomAnswerV(index)
-            }
+                if (r3.isChecked === true) {
+                    answersPoints = answersPoints + 10
+                    goodAnswer.isVisible = true
+                    quizView.isVisible = false
+                    r1.isVisible = false
+                    r2.isVisible = false
+                    r3.isVisible = false
+                    validateButton.isVisible = false
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.w(TAG, "Failed to read value.", error.toException())
-            }
-        })
+                }
+                if (r1.isChecked === true || r2.isChecked === true) {
+                    badAnswer.isVisible = true
+                    quizView.isVisible = false
+                    r1.isVisible = false
+                    r2.isVisible = false
+                    r3.isVisible = false
+                    validateButton.isVisible = false
+                }
+                sharedPref ()
+                index += 1
+                sharedPrefIndex()
+                validateButton.isVisible = false
 
-        r1.setOnClickListener {
-            if (r1.isChecked === true) {
-                r2.isChecked = false
-                r3.isChecked = false
             }
-        }
-
-        r2.setOnClickListener {
-            if (r2.isChecked === true) {
-                r1.isChecked = false
-                r3.isChecked = false
-            }
-        }
-
-        r3.setOnClickListener {
-            if (r3.isChecked === true) {
-                r1.isChecked = false
-                r2.isChecked = false
-            }
-        }
-
-        validateButton.setOnClickListener {
-            //index++ appel a la fct des jours
-            displayRandomQuestion(index)
-            displayRandomAnswer1(index)
-            displayRandomAnswer2(index)
-            displayRandomAnswerV(index)
-            if (r3.isChecked === true) {
-                answersPoints = answersPoints + 10
-                goodAnswer.isVisible = true
-                quizView.isVisible = false
-                pengouinGood.isVisible = true
-                r1.isVisible = false
-                r2.isVisible = false
-                r3.isVisible = false
-            }
-            if (r1.isChecked === true || r2.isChecked === true) {
-                badAnswer.isVisible = true
-                quizView.isVisible = false
-                r1.isVisible = false
-                r2.isVisible = false
-                r3.isVisible = false
-            }
-            validateButton.isVisible = false
+        }else
+        {
+            //Si le quiz a deja été fait
+            setContentView(R.layout.activity_quiz_done)
         }
     }
-
-  /* override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-       Log.d("testActivity","ok")
-        if (resultCode == RESULT_OK) {
-            Log.d("testActivity2","ok")
-
-            date = data?.getIntExtra("date", 0)!!
-            val sharedPref = thisActivity?.getPreferences(Context.MODE_PRIVATE) ?: return
-            with (sharedPref.edit()) {
-                putInt("date", date)
-                commit()
-            }
-            //Toast.makeText(this,"Pref : "+date,Toast.LENGTH_LONG).show()
-        }
-    }*/
-
 
     private fun displayRandomTips() {
         tipsView.isVisible = true
@@ -251,7 +232,6 @@ class QuizActivity : AppCompatActivity() {
 
     private fun displayDate() : Int
     {
-
         val formatter = SimpleDateFormat("dd/MM/yyyy/H/mm/ss")
         var dateString = formatter.format(currentDate)
         val components = dateString.split("/")
@@ -264,7 +244,26 @@ class QuizActivity : AppCompatActivity() {
         var day = components[0].toInt()
 
         //Toast.makeText(this,"Nous sommes le $day $month $year"+"Il est $hour $minutes $seconds",Toast.LENGTH_LONG).show()
-
-        return day
+        var dateJour = day+(month*100)+(year*1000)
+        return dateJour
     }
+
+    private fun sharedPref ()
+    {
+        val sharedPref = thisActivity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putInt("date", date)
+            commit()
+        }
+    }
+
+    private fun sharedPrefIndex ()
+    {
+        val sharedPrefIndex = thisActivity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPrefIndex.edit()) {
+            putInt("index", index)
+            commit()
+        }
+    }
+
 }
